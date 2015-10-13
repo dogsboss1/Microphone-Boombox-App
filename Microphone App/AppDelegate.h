e52cbd75da09f5ba36e461f0649cef3340c8737d
Felix King <felixking@felixs-macbook-pro.local>
2015-10-13 18:33:26 +0800

    Initial Commit

---
+++ Microphone App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Microphone App
+//
+//  Created by felix king on 13/10/2015.
+//  Copyright © 2015 Felix King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
